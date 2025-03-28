f52eb91a7328b58b2ed1074198b428dcf96609d1
Roseherd <richardkobbyadd@gmail.com>
2025-03-28 21:21:10 +0000

    adding all my app files

--- src/components/inventory_tracking.py
+++ src/components/inventory_tracking.py
@@ -9,7 +9,7 @@ from twilio.rest import Client
 TWILIO_SID = os.getenv("TWILIO_ACCOUNT_SID")
 TWILIO_AUTH_TOKEN = os.getenv("TWILIO_ACCOUNT_AUTH_TOKEN")
 TWILIO_WHATSAPP_NUMBER = "whatsapp:+14155238886"
-PHARMACY_WHATSAPP_NUMBER = "whatsapp:your_pharmacy_number"
+PHARMACY_WHATSAPP_NUMBER = "whatsapp:+233539681341"
 
 
 
